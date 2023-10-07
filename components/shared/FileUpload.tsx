410c168de86ccf1ed9c0082bdad5f63ae736b4ca
Samuel Esplana <samuelesplana.dev@gmail.com>
2023-10-07 17:21:27 +0800

    modify endpoint props

--- components/shared/FileUpload.tsx
+++ components/shared/FileUpload.tsx
@@ -7,12 +7,13 @@ import { toast } from "sonner";
 
 type FileUploadProps = {
   onChange: (url?: string) => void;
+  endpoint: "profileImageUploader" | "postImageUploader";
 };
 
-const FileUpload = ({ onChange }: FileUploadProps) => {
+const FileUpload = ({ onChange, endpoint }: FileUploadProps) => {
   return (
     <UploadDropzone<OurFileRouter>
-      endpoint="profileImageUploader"
+      endpoint={endpoint}
       onClientUploadComplete={(res) => {
         if (res && res[0].url) {
           onChange(res[0].url);
